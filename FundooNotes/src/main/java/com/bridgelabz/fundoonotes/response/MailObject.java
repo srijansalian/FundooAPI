44e61f21682ef8330d5d428fec34277334481e64
srijansalian <srijansalian@gmail.com>
2020-02-24 11:02:33 +0530

    [Srijan].Updated in the label entity

--- FundooNotes/src/main/java/com/bridgelabz/fundoonotes/response/MailObject.java
+++ FundooNotes/src/main/java/com/bridgelabz/fundoonotes/response/MailObject.java
@@ -8,7 +8,7 @@ import lombok.Data;
 
 /**
  * 
- * @author Srijan Kumar 
+ * @author Srijan Kumar
  * 
  */
 
@@ -24,33 +24,5 @@ public class MailObject implements Serializable {
 	private String email;
 	private String subject;
 	private String message;
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSubject() {
-		return subject;
-	}
-
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
 
 }
